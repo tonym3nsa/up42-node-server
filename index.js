e38c50b8e08505f734250ffe8961f16b7e6555c4
Anthony Agyekum Mensah <tony.m3nsa@gmail.com>
2022-09-12 08:09:23 +0000

    docs: Updated README.md

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
-const request = require('request');
-const cors = require('cors');
+const request = require("request");
+const cors = require("cors");
 
 // Create Express Server
 const app = express();
@@ -12,23 +12,22 @@ const HOST = "localhost";
 const API_SERVICE_URL = "https://api.up42.com/marketplace";
 
 // Logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // CORS
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res, next) => {
-    res.send('hello world!')
+app.get("/", (req, res, next) => {
+  res.send("hello world!");
 });
 
 // Info GET endpoint
-app.get('/blocks', (req, res, next) => {
-    const url = API_SERVICE_URL + req.url;
-    req.pipe(request(url)).pipe(res);
+app.get("/blocks", (req, res, next) => {
+  const url = API_SERVICE_URL + req.url;
+  req.pipe(request(url)).pipe(res);
 });
 
 // Start the Proxy
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
-
